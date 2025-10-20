f5768f43ce2886821497cc225743440a367789a7
AntonZemskov <anton-zemskov@mascc.pro>
2025-10-20 23:49:07 +0300

    feat(interface, impl): add SortingStrategy and ThreadPoolSortingStrategy

---
+++ src/main/java/org/example/sorting/ThreadPoolSortingStrategy.java
@@ -0,0 +1,60 @@
+package org.example.sorting;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+public class ThreadPoolSortingStrategy<T> implements SortingStrategy<T> {
+    @Override
+    public List<T> sort(List<T> list, Comparator<T> comparator, ExecutorService executor) {
+        if (list == null || list.isEmpty() || list.size() == 1) return list;
+
+        int halfListCount = list.size() / 2;
+        List<T> leftPart = list.subList(0, halfListCount);
+        List<T> rightPart = list.subList(halfListCount, list.size());
+
+        Future<List<T>> leftFuture = executor.submit(() -> sort(new ArrayList<>(leftPart), comparator, executor));
+        Future<List<T>> rightFuture = executor.submit(() -> sort(new ArrayList<>(rightPart), comparator, executor));
+
+        try {
+            List<T> sortedLeftPart = leftFuture.get();
+            List<T> sortedRightPart = rightFuture.get();
+            return merge(sortedLeftPart, sortedRightPart, comparator);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<T> merge(List<T> leftPart, List<T> rightPart, Comparator<T> comparator) {
+        List<T> mergedList = new ArrayList<>();
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        while (leftIndex < leftPart.size() && rightIndex < rightPart.size()) {
+            T leftElement = leftPart.get(leftIndex);
+            T rightElement = rightPart.get(rightIndex);
+
+            if (comparator.compare(leftElement, rightElement) <= 0) {
+                mergedList.add(leftElement);
+                leftIndex++;
+            } else {
+                mergedList.add(rightElement);
+                rightIndex++;
+            }
+        }
+
+        while (leftIndex < leftPart.size()) {
+            T leftElement = leftPart.get(leftIndex++);
+            mergedList.add(leftElement);
+        }
+
+        while (rightIndex < rightPart.size()) {
+            T rightElement = rightPart.get(rightIndex++);
+            mergedList.add(rightElement);
+        }
+
+        return mergedList;
+    }
+}
