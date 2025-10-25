9c2aef6d63c190a70f1bf38130b9230c5cbd3c48
AntonZemskov <anton-zemskov@mascc.pro>
2025-10-25 12:38:55 +0300

    feat(EvenNumberSortingStrategy, AlgorithmSorting): add sorting by even number and wrapper enum class

--- src/test/java/org/example/sorting/MergeSortingStrategyTest.java
+++ src/test/java/org/example/sorting/MergeSortingStrategyTest.java
@@ -1,5 +1,6 @@
 package org.example.sorting;
 
+import org.example.sorting.entities.Cat;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -27,7 +28,7 @@ class MergeSortingStrategyTest {
     @Test
     void whenSortingStringNaturalOrder_thenResultIsListSortedByAsc() {
         List<String> actualList = new ArrayList<>(List.of("a", "f", "e", "h", "c", "b", "g", "d"));
-        MergeSortingStrategy<String> strategy = new MergeSortingStrategy<>();
+        BaseSortingStrategy<String> strategy = new BaseSortingStrategy<>();
         
         List<String> sortedList = strategy.sort(actualList, Comparator.naturalOrder(), executor);
         List<String> expectedList = List.of("a", "b", "c", "d", "e", "f", "g", "h");
@@ -38,7 +39,7 @@ class MergeSortingStrategyTest {
     @Test
     void whenSortingStringReverseOrder_thenResultIsListSortedByDesc() {
         List<String> actualList = new ArrayList<>(List.of("a", "f", "e", "h", "c", "b", "g", "d"));
-        MergeSortingStrategy<String> strategy = new MergeSortingStrategy<>();
+        BaseSortingStrategy<String> strategy = new BaseSortingStrategy<>();
         
         List<String> sortedList = strategy.sort(actualList, Comparator.reverseOrder(), executor);
         List<String> expectedList = List.of("h", "g", "f", "e", "d", "c", "b", "a");
@@ -49,7 +50,36 @@ class MergeSortingStrategyTest {
     @Test
     void whenSortingIntegerNaturalOrder_thenResultIsListSortedByAsc() {
         List<Integer> actualList = new ArrayList<>(List.of(2, 1, 5, 3, 9, 6, 10, 8, 4, 7));
-        MergeSortingStrategy<Integer> strategy = new MergeSortingStrategy<>();
+        BaseSortingStrategy<Integer> strategy = new BaseSortingStrategy<>();
+        
+        List<Integer> sortedList = strategy.sort(actualList, Comparator.naturalOrder(), executor);
+        List<Integer> expectedList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        
+        Assertions.assertEquals(expectedList, sortedList);
+    }
+    
+    @Test
+    void whenSortingCatByAge_thenResultIsListSortedByAsc() {
+        List<Cat> actualList = new ArrayList<>(List.of(new Cat.CatBuilder().setName("Васька").setAge(2).setBreed("Чеширский").build(),
+                                                       new Cat.CatBuilder().setName("Мурка").setAge(4).setBreed("Сибирский").build(),
+                                                       new Cat.CatBuilder().setName("Жучка").setAge(9).setBreed("Британский").build(),
+                                                       new Cat.CatBuilder().setName("Ночка").setAge(8).setBreed("Русский").build(),
+                                                       new Cat.CatBuilder().setName("Тучка").setAge(1).setBreed("Канадский").build()));
+        BaseSortingStrategy<Cat> strategy = new BaseSortingStrategy<>();
+        
+        List<Cat> sortedList = strategy.sort(actualList, Comparator.comparing(Cat::getAge), executor);
+        
+        System.out.println(sortedList);
+        
+        
+//        List<Cat> expectedList = List.of(10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+//        Assertions.assertEquals(expectedList, sortedList);
+    }
+    
+    @Test
+    void whenSortingPersonByAge_thenResultIsListSortedByAsc() {
+        List<Integer> actualList = new ArrayList<>(List.of(2, 1, 5, 3, 9, 6, 10, 8, 4, 7));
+        BaseSortingStrategy<Integer> strategy = new BaseSortingStrategy<>();
         
         List<Integer> sortedList = strategy.sort(actualList, Comparator.naturalOrder(), executor);
         List<Integer> expectedList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
@@ -60,7 +90,7 @@ class MergeSortingStrategyTest {
     @Test
     void whenSortingIntegerReverseOrder_thenResultIsListSortedByDesc() {
         List<Integer> actualList = new ArrayList<>(List.of(2, 1, 5, 3, 9, 6, 10, 8, 4, 7));
-        MergeSortingStrategy<Integer> strategy = new MergeSortingStrategy<>();
+        BaseSortingStrategy<Integer> strategy = new BaseSortingStrategy<>();
         
         List<Integer> sortedList = strategy.sort(actualList, Comparator.reverseOrder(), executor);
         List<Integer> expectedList = List.of(10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
@@ -71,7 +101,7 @@ class MergeSortingStrategyTest {
     @Test
     void whenSortingThrowException_thenResultHasRuntimeException() {
         List<Double> actualList = new ArrayList<>(List.of(2.1, 1.2, 5.6, 3.3, 9.1, 6.9, 10.2, 8.6, 4.3, 7.9));
-        MergeSortingStrategy<Double> strategy = new MergeSortingStrategy<>();
+        BaseSortingStrategy<Double> strategy = new BaseSortingStrategy<>();
         
         Assertions.assertThrows(RuntimeException.class,
                                 () -> strategy.sort(actualList, (c1, c2) -> {throw new IllegalArgumentException("Sorting failed...");}, executor));
