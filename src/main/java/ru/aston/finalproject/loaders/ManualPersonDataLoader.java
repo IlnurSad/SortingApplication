c7ed1679436c5fae694652e789bf23b25b62c359
ILnur <sadykovilnurr@gmail.com>
2025-10-29 14:02:43 +0300

    Returned the number of generated objects in the Random classes. Removed unused imports, extra spaces, etc.

--- src/main/java/ru/aston/finalproject/loaders/ManualPersonDataLoader.java
+++ src/main/java/ru/aston/finalproject/loaders/ManualPersonDataLoader.java
@@ -4,7 +4,6 @@ import ru.aston.finalproject.entity.Person;
 import ru.aston.finalproject.interfaces.DataLoader;
 import ru.aston.finalproject.interfaces.Validator;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
